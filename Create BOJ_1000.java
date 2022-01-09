b6e408087ecb8b98404e37960d553524a71866d9
cys11103 <67255965+cys11103@users.noreply.github.com>
2022-01-09 22:34:59 +0900

    [BOJ] A+B / 난이도 / 10분
    https://www.acmicpc.net/problem/1000

--- Create BOJ_1000.java
+++ Create BOJ_1000.java
@@ -1,11 +1,14 @@
+package namyoungStudy;
 
-class Main{
-  public static void main(String arg[]){
-    
-    Scanner sc = new Scanner(System.in);
-    int a, b;
-    a = sc.nextInt();
-    b = sc.nextInt();
-    System.out.printf("a+b = %d", a+b);
-  }
-}
+import java.util.Scanner;
+
+public class Main{
+	  public static void main(String arg[]){
+	    
+	    Scanner sc = new Scanner(System.in);
+	    int a, b;
+	    a = sc.nextInt();
+	    b = sc.nextInt();
+	    System.out.printf("a+b = %d", a+b);
+	  }
+	}
