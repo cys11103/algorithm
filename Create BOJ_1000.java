95deeddec3f06a49298dd89110dd6cf983b3eadd
cys11103 <67255965+cys11103@users.noreply.github.com>
2022-01-09 22:29:27 +0900

    Create BOJ_1000.java
    연결 테스트

---
+++ Create BOJ_1000.java
@@ -0,0 +1,12 @@
+
+class Main{
+  public static void main(String arg[]){
+    
+    Scanner sc = new Scanner(System.in);
+    
+    int a, b;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    System.out.printf("a+b = %d", a+b);
+  }
+}
